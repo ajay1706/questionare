2500c776a01c3eefea16cfab3d54049c096db698
ajay1706 <ajay97456@gmail.com>
2019-08-13 00:00:28 +0530

    answers

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 // ignore: must_be_immutable
 class Question extends StatelessWidget {
   final String questionText;
 
   Question(this.questionText);
 
-
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: Text(questionText,style: TextStyle(fontSize: 28.0),
-      textAlign: TextAlign.center,),
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28.0,
+        fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
