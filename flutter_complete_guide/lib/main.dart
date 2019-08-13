14d214ca41164c3a4619027ef3eff1da436a3035
ajay1706 <ajay97456@gmail.com>
2019-08-13 13:34:22 +0530

    writing code separately in widgets

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/result.dart';
 
-import 'package:flutter_complete_guide/question.dart';
-import './answer.dart';
+import './quiz.dart';
 
 void main() {
   runApp(new MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  final questions = const [
+  final _questions = const [
     {
       'questionText': "What\'s your favorite color?",
       'answers': ['Black', 'Blue', 'Red', 'Green']
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
     });
 
     print(_questionIndex);
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print("We have more questions");
     }
   }
@@ -49,25 +49,13 @@ class _MyAppState extends State<MyApp> {
             title: Text("Sample"),
             centerTitle: true,
           ),
-          body: _questionIndex < questions.length
-              ? Column(
-                  children: [
-                    Question(
-                      questions[_questionIndex]['questionText'],
-                    ),
-                    ...(questions[_questionIndex]['answers'] as List<String>)
-                        .map((answer) {
-                      return Answer(_answerQuestion, answer);
-                    }).toList()
-                  ],
+          body: _questionIndex < _questions.length
+              ? Quiz(
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                  questions: _questions,
                 )
-              : Center(
-                  child: Text(
-                    "You did it",
-                    style:
-                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.w900),
-                  ),
-                ),
+              : Result()
         ));
   }
 }
